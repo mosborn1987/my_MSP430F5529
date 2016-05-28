c7a83e1d74038cdc39e5574c7ff5be35e74c88a4
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-05-27 23:32:01 -0400

    SELECTED THE FILE TYPES TO IGNORE.

--- main.c
+++ main.c
@@ -4,5 +4,21 @@ int main(void) {
 
     WDT_A_hold(WDT_A_BASE);
 
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN7);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN7);
+    while(1)
+    {
+    	GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    	GPIO_toggleOutputOnPin(GPIO_PORT_P4, GPIO_PIN7);
+    	__delay_cycles(40000);
+    	__delay_cycles(40000);
+    	__delay_cycles(40000);
+
+    }
+
     return (0);
 }
+
+
