f71442c0770d76e37416b786f38fe5b79075280c
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-05-29 12:54:17 -0400

    THIS IS THE LAST PUSH BEFORE I MAKE A BIG SCARRY ATTEMPT TO CONVERT C++ CODE TO C EQUIVALENCE.

--- main.c
+++ main.c
@@ -1,24 +1,73 @@
 #include "driverlib.h"
+#include <usci_a_uart.h>
+#include <msp430.h>
+#include <msp430f5529.h>
+#include <intrinsics.h>
 
 int main(void) {
 
+	int BAUD_RATE = 9600;
+	uint16_t PRESCALE_CLOCK_DIVIDER = 1;
     WDT_A_hold(WDT_A_BASE);
 
-    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN7);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN7);
-    while(1)
-    {
-    	GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
-    	GPIO_toggleOutputOnPin(GPIO_PORT_P4, GPIO_PIN7);
-    	__delay_cycles(40000);
-    	__delay_cycles(40000);
-    	__delay_cycles(40000);
 
-    }
+	if ( STATUS_FAIL == USCI_A_UART_init (
+			__MSP430_BASEADDRESS_USCI_A0__,
+			USCI_A_UART_CLOCKSOURCE_ACLK,
+			PRESCALE_CLOCK_DIVIDER,
+			UCS_getSMCLK(),//FIRST MOD
+			BAUD_RATE,//SECOND MOD
+			USCI_A_UART_NO_PARITY,//PARITY
+			USCI_A_UART_LSB_FIRST,//MSB OR LSB
+			USCI_A_UART_ONE_STOP_BIT, // STOP BITS
+			USCI_A_UART_MODE, // UART MODE
+			USCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION))
 
-    return (0);
+	{
+		return;
+	}
+
+
+
+	//Enable UART module for operation
+	UART_enable (__MSP430_BASEADDRESS_USCI_A0__);
+
+	//Enable Receive Interrupt
+	UART_enableInterrupt (__MSP430_BASEADDRESS_USCI_A0__, UCRXIE);
+
+	//Transmit data
+	UART_transmitData(__MSP430_BASEADDRESS_USCI_A0__, transmitData++);
+
+	// Enter LPM3, interrupts enabled
+	__bis_SR_register(LPM3_bits + GIE);
+	__no_operation();
 }
 
 
+//******************************************************************************
+//
+// This is the USCI_A0 interrupt vector service routine.
+//
+//******************************************************************************
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+{
+switch(__even_in_range(UCA0IV,4))
+{
+// Vector 2 - RXIFG
+case 2:
+// Echo back RXed character, confirm TX buffer is ready first
+// USCI_A0 TX buffer ready?
+while (!UART_interruptStatus(__MSP430_BASEADDRESS_USCI_A0__,
+UCTXIFG)
+);
+//Receive echoed data
+receivedData = UART_receiveData(__MSP430_BASEADDRESS_USCI_A0__);
+//Transmit next data
+UART_transmitData(__MSP430_BASEADDRESS_USCI_A0__,
+transmitData++
+);
+break;
+default: break;
+}
+}
