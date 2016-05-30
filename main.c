8ebfe119af6366b386a39d8a85b12ee4529d68f4
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-05-30 13:32:51 -0400

    UART FUNCTIONS WORKING

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <msp430.h>
 #include <UART.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -7,10 +8,13 @@ int main(void)
 
 	UART_init();
 
-	char valueX[10];
-	valueX[1]= UART_get_single_char();
-	valueX[2] = UART_get_single_char();
-
+	UART_get_array(10, UART_buffer);
+	UARTSendArray(UART_buffer, 10);
+//
+//	UART_buffer[] = "Hello World!";
+//	sprintf(UART_buffer, "hello world!n");
+//	UARTSendArray(UART_buffer, 12);
+//	UARTSendArray(UART_buffer, 12);
 
 	__bis_SR_register(LPM0_bits + GIE); // Enter LPM0, interrupts enabled
 	__no_operation(); // For debugger
