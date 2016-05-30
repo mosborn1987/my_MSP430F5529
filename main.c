4a513fdf728820747f40151c19f9999d38f08b62
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-05-30 11:49:41 -0400

    THE UART IS WORKING NOW.

--- main.c
+++ main.c
@@ -1,73 +1,40 @@
-#include "driverlib.h"
-#include <usci_a_uart.h>
 #include <msp430.h>
-#include <msp430f5529.h>
-#include <intrinsics.h>
 
-int main(void) {
+int main(void)
+{
+WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+P4SEL = BIT5+BIT4; // P3.4,5 = USCI_A0 TXD/RXD
+UCA1CTL1 |= UCSWRST; // **Put state machine in reset**
+UCA1CTL1 |= UCSSEL_2; // SMCLK
+UCA1BR0 = 6; // 1MHz 9600 (see User's Guide)
+UCA1BR1 = 0; // 1MHz 9600
+UCA1MCTL = UCBRS_0 + UCBRF_13 + UCOS16; // Modln UCBRSx=0, UCBRFx=0,
+// over sampling
+UCA1CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+UCA1IE |= UCRXIE; // Enable USCI_A0 RX interrupt
 
-	int BAUD_RATE = 9600;
-	uint16_t PRESCALE_CLOCK_DIVIDER = 1;
-    WDT_A_hold(WDT_A_BASE);
-
-
-	if ( STATUS_FAIL == USCI_A_UART_init (
-			__MSP430_BASEADDRESS_USCI_A0__,
-			USCI_A_UART_CLOCKSOURCE_ACLK,
-			PRESCALE_CLOCK_DIVIDER,
-			UCS_getSMCLK(),//FIRST MOD
-			BAUD_RATE,//SECOND MOD
-			USCI_A_UART_NO_PARITY,//PARITY
-			USCI_A_UART_LSB_FIRST,//MSB OR LSB
-			USCI_A_UART_ONE_STOP_BIT, // STOP BITS
-			USCI_A_UART_MODE, // UART MODE
-			USCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION))
-
-	{
-		return;
-	}
-
-
-
-	//Enable UART module for operation
-	UART_enable (__MSP430_BASEADDRESS_USCI_A0__);
-
-	//Enable Receive Interrupt
-	UART_enableInterrupt (__MSP430_BASEADDRESS_USCI_A0__, UCRXIE);
-
-	//Transmit data
-	UART_transmitData(__MSP430_BASEADDRESS_USCI_A0__, transmitData++);
-
-	// Enter LPM3, interrupts enabled
-	__bis_SR_register(LPM3_bits + GIE);
-	__no_operation();
+__bis_SR_register(LPM0_bits + GIE); // Enter LPM0, interrupts enabled
+__no_operation(); // For debugger
 }
 
-
-//******************************************************************************
-//
-// This is the USCI_A0 interrupt vector service routine.
-//
-//******************************************************************************
-#pragma vector=USCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
-{
-switch(__even_in_range(UCA0IV,4))
-{
-// Vector 2 - RXIFG
-case 2:
 // Echo back RXed character, confirm TX buffer is ready first
-// USCI_A0 TX buffer ready?
-while (!UART_interruptStatus(__MSP430_BASEADDRESS_USCI_A0__,
-UCTXIFG)
-);
-//Receive echoed data
-receivedData = UART_receiveData(__MSP430_BASEADDRESS_USCI_A0__);
-//Transmit next data
-UART_transmitData(__MSP430_BASEADDRESS_USCI_A0__,
-transmitData++
-);
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+switch(__even_in_range(UCA1IV,4))
+{
+case 0:break; // Vector 0 - no interrupt
+case 2: // Vector 2 - RXIFG
+while (!(UCA1IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+UCA1TXBUF = UCA1RXBUF; // TX -> RXed character
 break;
+case 4:break; // Vector 4 - TXIFG
 default: break;
 }
 }
