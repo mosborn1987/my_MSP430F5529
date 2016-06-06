1280e57d82e2f7be4f3399e6b4e33cf12fb16c3b
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-06-05 21:33:20 -0400

    digi-pot api almost complete

--- main.c
+++ main.c
@@ -7,7 +7,13 @@ void main(void)
 {
     //Stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
-    POT_DEMO();
+    init_SPI_B0();
+    POT_write_data(0x3F);
+    while(1)
+    {
+
+    };
+//    POT_DEMO();
 
 
 
