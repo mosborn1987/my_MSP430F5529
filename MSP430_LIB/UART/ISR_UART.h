99f03c7a294ece92b300e68127891f6a58a99b40
Mario Osborn <mosborn1987@knights.ucf.edu>
2016-06-12 14:57:54 -0400

    Cleaned Up Code. - Everything is still working

--- MSP430_LIB/UART/ISR_UART.h
+++ MSP430_LIB/UART/ISR_UART.h
@@ -16,6 +16,42 @@ static volatile char data;
 
 // Echo back RXed character, confirm TX buffer is ready first
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+	switch(__even_in_range(UCA0IV,4))
+	{
+		// Vector 0 - no interrupt
+		case 0:break;
+
+		// Vector 2 - RXIFG
+		case 2:
+		{
+			// USCI_A0 TX buffer ready?
+			while (!(UCA1IFG&UCTXIFG));
+			UCA0TXBUF = UCA0RXBUF; // TX -> RXed character
+			data = UCA0RXBUF;
+			break;
+		}
+
+
+		// Vector 4 - TXIFG
+		case 4:break;
+
+		default: break;
+
+	}
+	_BIC_SR(LPM0_EXIT);
+}
+
+
+// Echo back RXed character, confirm TX buffer is ready first
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector=USCI_A1_VECTOR
 __interrupt void USCI_A1_ISR(void)
 #elif defined(__GNUC__)
@@ -27,42 +63,29 @@ void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
 
 	switch(__even_in_range(UCA1IV,4))
 	{
-		case 0:break; // Vector 0 - no interrupt
-		case 2: // Vector 2 - RXIFG
-		while (!(UCA1IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
-		UCA1TXBUF = UCA1RXBUF; // TX -> RXed character
-		data = UCA1RXBUF;
-		break;
+		// Vector 0 - no interrupt
+		case 0:break;
+
+		// Vector 2 - RXIFG
+		case 2:
+		{
+			// USCI_A0 TX buffer ready?
+			while (!(UCA1IFG&UCTXIFG));
+
+			// TX -> RXed character
+			UCA1TXBUF = UCA1RXBUF;
+
+			data = UCA1RXBUF;
+			break;
+		}
+
+
 		case 4:break; // Vector 4 - TXIFG
 		default: break;
 	}
 	_BIC_SR(LPM0_EXIT);
 }
 
-// Echo back RXed character, confirm TX buffer is ready first
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-
-	switch(__even_in_range(UCA0IV,4))
-	{
-		case 0:break; // Vector 0 - no interrupt
-		case 2: // Vector 2 - RXIFG
-		while (!(UCA1IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
-		UCA0TXBUF = UCA0RXBUF; // TX -> RXed character
-		data = UCA0RXBUF;
-		break;
-		case 4:break; // Vector 4 - TXIFG
-		default: break;
-	}
-	_BIC_SR(LPM0_EXIT);
-}
 
 
 #endif /* DRIVERLIB_ISR_UART_H_ */
